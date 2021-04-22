beaa04c46b0efe46e54b1125e6adc85509ffacee
mychi darko <mickdd22@gmail.com>
2021-04-22 14:08:30 +0000

    added form hook and backend connection

---
+++ client/src/config/constants.ts
@@ -0,0 +1,2 @@
+export const APP_TITLE = "Soko App";
+export const API_URL = "http://localhost:3301";
--- client/src/views/Register/components/Form.tsx
+++ client/src/views/Register/components/Form.tsx
@@ -1,17 +1,67 @@
+import axios from "axios";
 import { useState } from "react";
 import Button from "../../../components/Button";
+import { API_URL } from "../../../config/constants";
+import useInput from "../../../utils/hooks/useInput";
 
 const Form = () => {
     const [loading, setLoading] = useState(false);
+    const [errors, setErrors] = useState({ phone: "", password: "" });
+
+    const { value: phone, bind: bindPhone } = useInput("");
+    const { value: password, bind: bindPassword } = useInput("");
+
+    const validation = () => {
+        let isValid = true;
+        let phoneError = "";
+        let passwordError = "";
+
+        if (phone.length < 10) {
+            phoneError = "Enter a valid phone number!";
+            isValid = false;
+        } else {
+            phoneError = "";
+        }
+
+        if (password.length < 8) {
+            passwordError = "Password should be at lease 8 characters!";
+            isValid = false;
+        } else {
+            passwordError = "";
+        }
+    
+        setErrors({
+            phone: phoneError,
+            password: passwordError,
+        });
+
+        return isValid;
+    };
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
 
+        if (!validation()) {
+            return;
+        }
+
         setLoading(true);
 
-        setTimeout(() => {
+        const data = { phone, password };
+
+        axios.post(`${API_URL}/auth/register`, data).then((res) => {
+            if (res.data.status === "success") {
+                const { token } = res.data.data;
+
+                // save token in storage
+                window.localStorage["token"] = token;
+            }
+
             setLoading(false);
-        }, 3000);
+        }).catch((err) => {
+            console.log(err);
+            setLoading(false);
+        });
     };
 
     return (
@@ -20,8 +70,36 @@ const Form = () => {
             className="register-page__form"
         >
             <h2 className="register-page__form__title">Register an account.</h2>
-            {/*  */}
-            <Button loading={loading}>Register</Button>
+            <div className="form-group flex-col mt:_2">
+                <label>Phone</label>
+                <input
+                    type="text"
+                    className="form-input"
+                    name="phone"
+                    placeholder="0560265879"
+                    {...bindPhone}
+                />
+                <small className="form-errors">{errors.phone}</small>
+            </div>
+            <div className="form-group flex-col mt:_1">
+                <label>Password</label>
+                <input
+                    type="text"
+                    className="form-input"
+                    name="password"
+                    placeholder="*******"
+                    {...bindPassword}
+                />
+                <small className="form-errors">{errors.password}</small>
+            </div>
+            <div className="form-group mt:_2">
+                <Button
+                    loading={loading}
+                    disabled={(phone.length === 0 || password.length === 0)}
+                >
+                    Register
+                </Button>
+            </div>
         </form>
     );
 };

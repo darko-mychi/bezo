c0b29225838a2511c1264a43d99997d281e3bebc
mychi darko <mickdd22@gmail.com>
2021-04-22 12:18:45 +0000

    implemented user model in controller

--- api/app/controllers/auth.controller.ts
+++ api/app/controllers/auth.controller.ts
@@ -1,12 +1,19 @@
 import { RequestHandler } from "express";
 import Responder from "@/lib/responder";
+import User from "../models/user.model";
 
 export default class AuthController {
-    public static register: RequestHandler = (req, res) => {
+    public static register: RequestHandler = async (req, res) => {
         const { phone, password } = req.body;
 
-        Responder.success(res, {
-            phone, password
-        });
+        try {
+            const user = await User.create({ phone, password });
+            
+            Responder.success(res, user);
+        } catch (error) {
+            console.log(error.message, error.code);
+
+            Responder.error(res, "An error occured!");
+        }
     };
 }

43527be1c66295593ab8734dc6eb259318317fe8
mychi darko <mickdd22@gmail.com>
2021-04-22 12:14:22 +0000

    added password hashing

--- api/app/models/user.model.ts
+++ api/app/models/user.model.ts
@@ -1,5 +1,12 @@
+import { genSalt, hash } from "bcrypt";
 import { model } from "mongoose";
 import UserSchema from "../database/schema/user.schema";
+
+UserSchema.pre("save", async function(next) {
+    const salt = await genSalt();
+    this.password = await hash(this.password, salt);
+    next();
+});
 
 const User = model("user", UserSchema);
 
